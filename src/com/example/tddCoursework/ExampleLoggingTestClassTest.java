e07bb88ec38883d8285e8c682616854573de527f
John Shearer <john.shearer@ncl.ac.uk>
2013-11-20 02:02:20 +0000

    initial commit

---
+++ src/com/example/tddCoursework/ExampleLoggingTestClassTest.java
@@ -0,0 +1,19 @@
+package com.example.tddCoursework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleLoggingTestClassTest extends AbstractLoggingTestClass {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+
+}
